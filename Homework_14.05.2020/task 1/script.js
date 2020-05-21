efdf9ae6f71b2be162a562ee84534b05b3ece463
Paskalik <Paskalik1983@gmail.com>
2020-05-21 22:17:35 +0300

    20200521

---
+++ Homework_14.05.2020/task 1/script.js
@@ -0,0 +1,19 @@
+const $menu = $('li:first-child')
+
+const $list = $('ul');
+
+$menu.click(function () {
+    $list.toggleClass('full');
+    if ($list.hasClass('full')) {
+        const $items = $('li:not(:first-child)');
+        $items.each(function() {
+            $(this).hover(
+                () => {
+                    $(this).css('background-color','dimgrey')
+                },
+                () => {
+                    $(this).css('background-color','gray')
+                })
+        })
+    }
+})
