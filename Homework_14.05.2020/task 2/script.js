d620dca45bafbbf9596208ca4d2a6ca59f77f994
Paskalik <Paskalik1983@gmail.com>
2020-05-21 23:46:41 +0300

    20200521

---
+++ Homework_14.05.2020/task 2/script.js
@@ -0,0 +1,48 @@
+const $red = $('#red')
+const $green = $('#green')
+const $orange = $('#orange')
+const $yellow = $('#yellow')
+
+const $redAll = $('.red')
+const $greenAll = $('.green')
+const $orangeAll = $('.orange')
+const $yellowAll = $('.yellow')
+
+$red.click(function () {
+    $clearShadow()
+    $redAll.addClass('shadow')
+})
+
+$green.click(function () {
+    $clearShadow()
+    $greenAll.addClass('shadow')
+})
+
+$orange.click(function () {
+    $clearShadow()
+    $orangeAll.addClass('shadow')
+})
+
+$yellow.click(function () {
+    $clearShadow()
+    $yellowAll.addClass('shadow')
+})
+
+$green.click(function () {
+    $greenAll.addClass('shadow')
+})
+
+$orange.click(function () {
+    $orangeAll.addClass('shadow')
+})
+
+$yellow.click(function () {
+    $yellowAll.addClass('shadow')
+})
+
+function $clearShadow() {
+    const $shadow = $('.shadow');
+    if ($shadow) {
+        $shadow.removeClass('shadow')
+    }
+}
