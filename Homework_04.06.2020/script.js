cc60a1c26c03461ec7d61981a47feb8e8a25e936
Paskalik <Paskalik1983@gmail.com>
2020-06-13 01:08:35 +0300

    commit

--- Homework_04.06.2020/script.js
+++ Homework_04.06.2020/script.js
@@ -4,6 +4,7 @@
 
 let $header = $('#header')
 let $form = $('#form')
+let $data
 
 $(document).ready(function($) {
     $form.parsley()
@@ -16,6 +17,10 @@ $(document).ready(function($) {
         $input.each(function () {
             $formatError($(this))
         })
+        if ($('.parsley-error').length === 0) {
+            $data = $saveInfo()
+            $showRegisterResult()
+        }
     })
 })
 
@@ -24,45 +29,49 @@ function $registerForm() {
     $form.append('<label for="username" class="name">Username:</label><input id="username" class="name">')
     $('.name').wrapAll('<div class="row">')
     let $username = $('#username')
-    $username.attr('data-parsley-required','true')
-    $username.attr('data-parsley-trigger','focusout')
     $username.attr('data-parsley-minlength','4')
     $username.attr('data-parsley-maxlength','20')
-    $username.wrap('<div class="col-75">')
-    $('label.name').wrap('<div class="col-25">')
-    $form.append('<label for="email" type="email" class="email">Email:</label><input id="email" class="email">')
+    $form.append('<label for="email" type="email" class="email">Email:</label><input id="email" class="email" type="email">')
     $('.email').wrapAll('<div class="row">')
-    let $email = $('#email')
-    $email.wrap('<div class="col-75">')
-    $email.attr('data-parsley-required','true')
-    $email.attr('data-parsley-trigger','focusout')
-    $('label.email').wrap('<div class="col-25">')
-    $form.append('<label for="password" class="pass">Password:</label><input id="password" class="pass">')
+    $form.append('<label for="password" class="pass">Password:</label><input id="password" class="pass" type="password">')
     $('.pass').wrapAll('<div class="row">')
     let $password = $('#password')
-    $password.wrap('<div class="col-75">')
-   $password.attr('data-parsley-required','true')
-    $password.attr('data-parsley-trigger','focusout')
-    $('label.pass').wrap('<div class="col-25">')
-    $form.append('<label for="repeat" class="repeat">Repeat:</label><input id="repeat" class="repeat">')
+    $password.attr('data-parsley-minlength','6')
+    $password.attr('data-parsley-maxlength','30')
+    $form.append('<label for="repeat" class="repeat">Repeat:</label><input id="repeat" class="repeat" type="password">')
     $('.repeat').wrapAll('<div class="row">')
     let $repeat = $('#repeat')
-    $repeat.wrap('<div class="col-75">')
-    $repeat.attr('data-parsley-required','true')
-    $repeat.attr('data-parsley-equalto','#pass')
-    $repeat.attr('data-parsley-trigger','focusout')
-    $('label.repeat').wrap('<div class="col-25">')
+    $repeat.attr('data-parsley-equalto','#password')
     $form.append('<button type="submit">Register</button>')
+    let $input = $('input')
+    $input.attr('data-parsley-trigger','focusout')
+    $input.attr('data-parsley-required','true')
+    $input.wrap('<div class="col-75">')
+    $('label').wrap('<div class="col-25">')
+}
+
+function $saveInfo() {
+    return [$('#username').val(), $('#email').val(), $('#password').val()]
+}
+
+function $showRegisterResult() {
+    $form.append('<h4>Result</h4>')
+    let $table = $form.append('<table>')
+    $table.append('<tr><td>Username</td><td>' + $data[0] + '</td></tr>')
+    $table.append('<tr><td>Email</td><td>' + $data[1] + '</td></tr>')
+    $table.append('<tr><td>Password</td><td>' + $data[2] + '</td></tr>')
 }
 
 function $formatError(input) {
     let $label = $('label[for="' + input.prop('id') + '"]').text()
-    let text = $label.substring(0, $label.length - 1)
-    input.attr('data-parsley-required-message', text + ' is required')
-    input.attr('data-parsley-minlength-message', text + ' is required')
-    $form.width('80%')
-    input.removeClass('col-75').addClass('col-50')
-    $('button').width('64%')
+    let $text = $label.substring(0, $label.length - 1)
+    let $min = input.attr('data-parsley-minlength')
+    let $max = input.attr('data-parsley-maxlength')
+    input.attr('data-parsley-required-message', $text + ' is required')
+    input.attr('data-parsley-minlength-message', 'Min length is ' + $min + ' characters')
+    input.attr('data-parsley-maxlength-message', 'Max length is ' + $max + ' characters')
+    input.attr('data-parsley-type-message', 'Enter a valid email address')
+    input.attr('data-parsley-equalto-message', 'The password must match')
 }
 
 function $validateField(input) {
