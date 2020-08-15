c6960d56f833be9e380807698133af6a9de7f269
Paskalik <57663523+Paskalik@users.noreply.github.com>
2020-08-15 22:02:34 +0300

    commit

--- Homework_06.08.2020/pomodoro-app/src/app/pomodoro/pomodoro.component.ts
+++ Homework_06.08.2020/pomodoro-app/src/app/pomodoro/pomodoro.component.ts
@@ -8,16 +8,53 @@ import * as intervals from '../../environments/environment.interval';
 })
 export class PomodoroComponent implements OnInit {
   timeLeft: number = intervals.pomodoro;
+  timeLeftPomodoro: number = intervals.pomodoro;
+  timeLeftShort: number = intervals.short;
+  timeLeftLong: number = intervals.long;
   interval: number;
+  count = 0;
 
 
   constructor() { }
   startTimer(): void {
+    this.timeLeft = this.timeLeftPomodoro;
     this.interval = setInterval(() => {
       if (this.timeLeft > 0) {
         this.timeLeft--;
       } else {
-        this.timeLeft = 60;
+        this.count++;
+        if (this.count < 4) {
+          clearInterval(this.interval);
+          this.startShortBreak();
+        } else {
+          this.count = 0;
+          clearInterval(this.interval);
+          this.startLongBreak();
+        }
+      }
+    }, 1000);
+  }
+
+  startShortBreak(): void {
+    this.timeLeft = this.timeLeftShort;
+    this.interval = setInterval(() => {
+      if (this.timeLeft > 0) {
+        this.timeLeft--;
+      } else {
+        clearInterval(this.interval);
+        this.startTimer();
+      }
+    }, 1000);
+  }
+
+  startLongBreak(): void {
+    this.timeLeft = this.timeLeftLong;
+    this.interval = setInterval(() => {
+      if (this.timeLeft > 0) {
+        this.timeLeft--;
+      } else {
+        clearInterval(this.interval);
+        this.startTimer();
       }
     }, 1000);
   }
--- Homework_06.08.2020/pomodoro-app/src/environments/environment.interval.ts
+++ Homework_06.08.2020/pomodoro-app/src/environments/environment.interval.ts
@@ -1,5 +1,5 @@
-export const pomodoro = 120; // 1500;
+export const pomodoro = 20; // 1500;
 
-export const shot = 300;
+export const short = 5;
 
-export const long = 1200;
+export const long = 10;
