f8a31a9e01998060194424beee13e378fc946669
Paskalik <57663523+Paskalik@users.noreply.github.com>
2020-10-15 02:40:57 +0300

    Update BottomButtons.js

--- Diploma/my-store/src/store/BottomButtons.js
+++ Diploma/my-store/src/store/BottomButtons.js
@@ -2,7 +2,6 @@ import React from "react";
 import "../index.css";
 import Button from '@material-ui/core/Button';
 import AddIcon from '@material-ui/icons/Add';
-import SearchIcon from '@material-ui/icons/Search';
 import 'reactjs-popup/dist/index.css';
 import AddEditProduct from "./AddEditProduct";
 
@@ -31,10 +30,6 @@ export default class BottomButtons extends React.Component {
         })
     }
 
-    handleSearch() {
-        this.props.updateSearch();
-    }
-
 
     render() {
         return (
@@ -49,16 +44,6 @@ export default class BottomButtons extends React.Component {
                     Add
                 </Button>
                 <AddEditProduct open={this.state.open} close={this.close} updateSave={this.props.updateSave} categories = {this.props.categories} storages = {this.props.storages} new={true}/>
-                <Button
-                    variant="outlined"
-                    color="primary"
-                    size="large"
-                    type="search"
-                    startIcon={<SearchIcon />}
-                    onClick={this.handleSearch}
-                >
-                    Search
-                </Button>
             </div>
         )
     }
